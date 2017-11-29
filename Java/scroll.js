c6e2471e4cfb9ed06e00c1569b208a36560503f7
jokersgotaposse <jokersgotaposse@gmail.com>
2017-11-29 11:13:10 -0800

    image

--- Java/scroll.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-    $(window).scroll(function(e) {
-        var s = $(window).scrollTop(),
-            opacityVal = (s / 200);
-
-        $('.blurred-image').css('opacity', opacityVal);
-    });
-});
